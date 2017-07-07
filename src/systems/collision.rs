24ea4fd811ad3dcb083c4dee107b33f3300fb152
Toby Smyth <tobs169@gmail.com>
2017-07-07 20:07:36 +0100

    use dirty

--- src/systems/collision.rs
+++ src/systems/collision.rs
@@ -49,6 +49,37 @@ impl<'a> CollisionSystem {
         }
         world.update();
     }
+
+    fn basic_physics(&mut self,
+                     pos: &mut WriteStorage<'a, Pos>)
+                     -> bool {
+        let world = &mut self.0;
+        let mut dirty = false;
+        for (e1, e2, ca) in world.contact_pairs() {
+            let mut contacts = std::vec::Vec::new();
+            ca.contacts(&mut contacts);
+            for contact in contacts {
+                let mut move_vec = contact.normal * contact.depth * -0.5;
+                {
+                    if let Some(p1) = pos.get_mut(e1.data) {
+                        dirty = true;
+                        p1.x += move_vec.x;
+                        p1.y += move_vec.y;
+                    }
+                }
+                move_vec *= -1.0;
+                {
+                    if let Some(p2) = pos.get_mut(e2.data) {
+                        dirty = true;
+                        p2.x += move_vec.x;
+                        p2.y += move_vec.y;
+                    }
+                }
+            }
+        }
+        return dirty;
+
+    }
 }
 
 impl<'a> System<'a> for CollisionSystem {
@@ -60,33 +91,11 @@ impl<'a> System<'a> for CollisionSystem {
         let mut dirty = true;
         let mut i = 0;
         while i < 10 && dirty {
-            dirty = false;
             {
                 self.update_collisions(&ent, &pos, &col, &bounds);
             }
-            let world = &mut self.0;
-
-            for (e1, e2, ca) in world.contact_pairs() {
-                let mut contacts = std::vec::Vec::new();
-                ca.contacts(&mut contacts);
-                for contact in contacts {
-                    let mut move_vec = contact.normal * contact.depth * -0.5;
-                    {
-                        if let Some(p1) = pos.get_mut(e1.data) {
-                            dirty = true;
-                            p1.x += move_vec.x;
-                            p1.y += move_vec.y;
-                        }
-                    }
-                    move_vec *= -1.0;
-                    {
-                        if let Some(p2) = pos.get_mut(e2.data) {
-                            dirty = true;
-                            p2.x += move_vec.x;
-                            p2.y += move_vec.y;
-                        }
-                    }
-                }
+            {
+                dirty = self.basic_physics(&mut pos);
             }
             i += 1;
         }
