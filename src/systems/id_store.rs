41d1e1a3774072dd94593df756b624e03974e358
Toby Smyth <tobs169@gmail.com>
2017-07-14 10:04:31 +0100

    id map

--- src/systems/id_store.rs
+++ src/systems/id_store.rs
@@ -1,8 +1,10 @@
 
 use std::collections::HashSet;
+use std::collections::HashMap;
+use std::hash::Hash;
 use std::cmp::min;
 
-struct IdStore(HashSet<usize>, usize);
+pub struct IdStore(HashSet<usize>, usize);
 
 impl IdStore {
     pub fn new() -> IdStore {
@@ -15,15 +17,38 @@ impl IdStore {
         id
     }
 
-    pub fn release(&mut self, id : usize) {
+    pub fn release(&mut self, id: usize) {
         self.0.remove(&id);
         self.1 = min(id, self.1);
     }
-    fn next_after_add(&self, added : usize) -> usize{
-        let mut next = added+1;
-            while self.0.contains(&next){
-                next += 1;
-            }
-            next
+    fn next_after_add(&self, added: usize) -> usize {
+        let mut next = added + 1;
+        while self.0.contains(&next) {
+            next += 1;
         }
+        next
+    }
+}
+
+pub struct IdMap<T>(IdStore, HashMap<T, usize>);
+
+impl<T> IdMap<T>
+    where T: Eq + Hash + Copy 
+{
+    pub fn new() -> IdMap<T> {
+        IdMap(IdStore::new(), HashMap::new())
+    }
+
+    pub fn get(&mut self, id: T) -> &mut usize {
+       let m = &mut self.1;
+       let s = &mut self.0;
+       let i = m.entry(id).or_insert_with(|| {s.get()});
+       i
+    }
+
+    pub fn release(&mut self, id: T) {
+        if let Some(i) = self.1.remove(&id) {
+            self.0.release(i);
+        }
+    }
 }
